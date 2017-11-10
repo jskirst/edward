6ee65a495f372bbe1d1d836db320878ebe7c8c99
Jonathan Kirst <jskirst@gmail.com>
2017-11-10 11:28:31 +0800

    Adding callout failure mode

--- lib/prompt.rb
+++ lib/prompt.rb
@@ -7,7 +7,8 @@ class Prompt
   attr_reader :step, :user_facts
 
   delegate :text, :token, :cta, :cta_class, :cta_href,
-    :callout, :callout_body, :callout_method, to: :step
+    :callout, :callout_body, :callout_method, :callout_success,
+    :callout_failure_text, :callout_failure_cta, to: :step
 
   def initialize(step:, user_facts:)
     @step = step
@@ -19,22 +20,33 @@ class Prompt
   end
 
   def to_h
+    facts
     @hsh ||= begin
-      hsh = { text: text }
-      hsh.merge!(token: token) if token
-      hsh.merge!(cta: cta) if cta
-      hsh.merge!(cta_class: cta_class) if cta_class
-      hsh.merge!(cta_href: cta_href) if cta_href
-      hsh[:parts] = PartsCollection.new(text, answers, facts).to_a
+      hsh = { token: token }
+      if @callout_successful
+        hsh.merge!(text: text)
+        hsh.merge!(cta: cta) if cta
+        hsh.merge!(cta_class: cta_class) if cta_class
+        hsh.merge!(cta_href: cta_href) if cta_href
+      else
+        hsh.merge!(text: callout_failure_text)
+        hsh.merge!(cta: callout_failure_cta)
+        hsh.merge!(cta: cta_class) if cta_class
+      end
+      hsh[:parts] = PartsCollection.new(hsh[:text], answers(text: hsh[:text]), facts).to_a
       hsh
     end
   end
 
-  def answers
+  def answers(text: text)
     @answers ||= AnswersCollection.new(step: step, text: text, facts: facts).to_h
   end
 
   def facts
-    @facts ||= FactsCollection.new(text, @user_facts, callout, callout_method, callout_body).to_h
+    @facts ||= begin
+      facts_collection = FactsCollection.new(text, @user_facts, callout, callout_method, callout_body, callout_success)
+      @callout_successful = facts_collection.make_callout
+      facts_collection.to_h
+    end
   end
 end
--- lib/prompt/facts_collection.rb
+++ lib/prompt/facts_collection.rb
@@ -1,16 +1,13 @@
 class FactsCollection
-  attr_reader :text, :user_facts, :callout, :callout_method, :callout_body
+  attr_reader :text, :user_facts, :callout, :callout_method, :callout_body, :callout_response, :callout_success
 
-  def initialize(text, user_facts, callout, callout_method, callout_body)
+  def initialize(text, user_facts, callout, callout_method, callout_body, callout_success)
     @text = text
     @user_facts = user_facts
     @callout = callout
     @callout_method = callout_method
     @callout_body = callout_body
-    if callout.present?
-      Rails.logger.info "Calling out to: #{callout}"
-      @callout_facts = make_callout
-    end
+    @callout_success = callout_success
   end
 
   def to_h
@@ -18,17 +15,26 @@ class FactsCollection
   end
 
   def make_callout
-    return unless callout
+    return true unless callout
     url_template = Liquid::Template.parse(callout)
     url = url_template.render(user_facts)
     if callout_method == "get"
-      HTTParty.get(url).parsed_response.symbolize_keys
+      @callout_response = HTTParty.get(url)
     else
       body_template = Liquid::Template.parse(callout_body)
       body = body_template.render(user_facts)
-
-      HTTParty.post(url, { body: body }).parsed_response&.symbolize_keys
+      @callout_response = HTTParty.post(url, { body: body }).parsed_response&.symbolize_keys
     end
+    @callout_facts = @callout_response.parsed_response&.symbolize_keys
+    return callout_successful?
+  end
+
+  private
+
+  def callout_successful?
+    return true unless callout_success
+    path = JsonPath.new(callout_success)
+    path.on(@callout_facts.to_json).first.present?
   end
 end
 
